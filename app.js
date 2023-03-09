dad2c35ce7b0c693f0d04e213f6e2f5fc56c02d7
tom-2810 <tom.poortman@hva.nl>
2023-03-09 17:41:19 +0100

    👷 toolboard -> ejs

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+// Maak een nieuwe express app
+const app = express()
+
+// Stel in hoe we express gebruiken
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+
+// Maak een route voor de index
+app.get('/', (request, response) => {
+      response.render('index')
+  })
+
+
+// Stel het poortnummer in en start express
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
